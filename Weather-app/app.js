d7bd4223ac40f5b562855e1f483fa9a521291c7a
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-17 14:59:39 +0600

    Intro to asynchronous behaviour

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -1,1 +1,14 @@
-console.log('Hellow world!!')
+//Asynchronous behaviour
+
+console.log('Starting')
+
+setTimeout(() => {
+    console.log('This is being printed in the console after 3 second') // This will print after 3 second when starting and stopping will already be printed
+
+},3000)
+
+setTimeout(()=> {
+    console.log('0 sec timer')
+},0)
+
+console.log('stopping')
