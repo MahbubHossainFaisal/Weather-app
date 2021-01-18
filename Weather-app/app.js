0b4e52636a2dfefd12374b7f5ff9c77a3db93e22
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-18 18:18:13 +0600

    Using geocoding api from mapbox website

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -6,3 +6,13 @@ request({ url: url, json: true }, (error,response) => {
    console.log('It is currently '+response.body.current.temp+ ' degrees out')
    console.log('Current weather is : ' +response.body.current.weather[0].main)
 })
+
+//Geocoding 
+
+const geocodeURL='https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFoYnViaG9zc2FpbiIsImEiOiJja2syaHFuZGgwYjdwMndudTI4ZHlwanp3In0.BeiFxca5C3lHuJjHnxsnfg&limit=1'
+
+request({url:geocodeURL,json: true},(error,response) => {
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+    console.log('Latitude: '+latitude+', Longitude: '+longitude)
+})
