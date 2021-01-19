7b87f6992a5232b4c0a6e9c4891bfa6e8fd6c706
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-19 23:18:41 +0600

    Converting geocoding api method into asynchronous callback function method

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -18,18 +18,44 @@ request({ url: url, json: true }, (error,response) => {
 //Geocoding 
 
 
-const geocodeURL='https://api.mapbox.com/geocoding/v5/mapbox.places/los%20Angeles.json?access_token=pk.eyJ1IjoibWFoYnViaG9zc2FpbiIsImEiOiJja2syaHFuZGgwYjdwMndudTI4ZHlwanp3In0.BeiFxca5C3lHuJjHnxsnfg&limit=1'
+// const geocodeURL='https://api.mapbox.com/geocoding/v5/mapbox.places/los%20Angeles.json?access_token=pk.eyJ1IjoibWFoYnViaG9zc2FpbiIsImEiOiJja2syaHFuZGgwYjdwMndudTI4ZHlwanp3In0.BeiFxca5C3lHuJjHnxsnfg&limit=1'
 
-request({url:geocodeURL,json: true},(error,response) => {
+// request({url:geocodeURL,json: true},(error,response) => {
 
-    if(error) {
-        console.log('Unable to connect to the map service!!!')
-    } else if(response.body.message || response.body.features.length===0) {
-        console.log('Unable to find location! Try again ')
-    } else {
-        const latitude = response.body.features[0].center[0]
-        const longitude = response.body.features[0].center[1]
-        console.log('Latitude: '+latitude+', Longitude: '+longitude)
-    }
+//     if(error) {
+//         console.log('Unable to connect to the map service!!!')
+//     } else if(response.body.message || response.body.features.length===0) {
+//         console.log('Unable to find location! Try again ')
+//     } else {
+//         const latitude = response.body.features[0].center[0]
+//         const longitude = response.body.features[0].center[1]
+//         console.log('Latitude: '+latitude+', Longitude: '+longitude)
+//     }
     
-}) 
+// }) 
+
+
+const geocode = (address,callback) => {
+
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibWFoYnViaG9zc2FpbiIsImEiOiJja2syaHFuZGgwYjdwMndudTI4ZHlwanp3In0.BeiFxca5C3lHuJjHnxsnfg&limit=1'
+
+    request({url: url, json: true},(error,response)=>{
+        if(error){
+            callback('Unable to connect to the location service!',undefined)
+        } else if(response.body.message || response.body.features.length===0){
+            callback('Unable to find location! Try again',undefined)
+        } else {
+            callback(undefined,{
+                latitude:response.body.features[0].center[0],
+                longitude:response.body.features[0].center[1],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+
+geocode('Himachal',(error,data) => {
+    console.log(error)
+    console.log(data)
+})
