90ae8716b66ba181c0397168b24533ca3d71e44c
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-20 08:08:34 +0600

    Callback chaining- Chaining forecast callback under geocode callback by using the geocode latitude and longitude value as forecast functions parameters

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -1,19 +1,20 @@
-const request = require('request')
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 
 
+geocode('Narayanganj',(error,locationData) => {
+    if(error){
+        return console.log(error)
+    }
 
-forecast('23.8103','90.4125',(error,data)=>{
-    console.log(error)
-    console.log(data)
-})
-
-
-
-
-geocode('Rajshahi',(error,data) => {
-    console.log(error)
-    console.log(data)
+    forecast(locationData.latitude,locationData.longitude,(error,forecastData)=>{
+        if(error){
+            return console.log(error)
+        }
+        console.log(locationData.location)
+        //console.log(forecastData)
+        console.log('Current tempurature is ' + forecastData.tempurature + ' degress')
+        console.log('Weather status: '+forecastData.weatherStatus)
+    })
 })
