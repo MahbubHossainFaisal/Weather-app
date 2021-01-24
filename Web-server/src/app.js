c711b81f4006bd2450446a3820ae03b915a3df16
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-24 20:55:37 +0600

    creating dynamic reusable hbs files in views(about,help)

--- Web-server/src/app.js
+++ Web-server/src/app.js
@@ -12,7 +12,23 @@ app.set('view engine','hbs')
 app.use(exrpess.static(publicDirectoryPath))
 
 app.get('', (req,res) => {
-    res.render('index')
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Mahbub Hossain Faisal'
+    })
+})
+
+app.get('/about', (req,res) =>{
+    res.render('about',{
+        title:'Weather App',
+        name: 'Mahbub Hossain Faisal'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        email: 'mahbubhossain249@gmail.com'
+    })
 })
 
 app.get('/weather', (req,res) => {
