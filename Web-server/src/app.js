717895d11a5faf38d42322c94449e0c8e43f2ab5
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-25 19:14:35 +0600

    Advanced Templating(Using partials)

--- Web-server/src/app.js
+++ Web-server/src/app.js
@@ -1,17 +1,20 @@
 const path = require('path') // Built in node module.So no need to install
 const exrpess = require('express')
-
+const hbs = require('hbs')
 
 const app = exrpess()
 
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsDirectoryPath = path.join(__dirname,'../templates')
+const viewsDirectoryPath = path.join(__dirname,'../templates/views')
+const partialsDirectoryPath = path.join(__dirname,'../templates/partials')
 
 
-//setup handlebar engine and views location
+
+//setup handlebar engine and views location and partials location
 app.set('view engine','hbs')
 app.set('views', viewsDirectoryPath)
+hbs.registerPartials(partialsDirectoryPath)
 
 //setup static directory to serve
 app.use(exrpess.static(publicDirectoryPath))
@@ -25,14 +28,16 @@ app.get('', (req,res) => {
 
 app.get('/about', (req,res) =>{
     res.render('about',{
-        title:'Weather App',
+        title:'About',
         name: 'Mahbub Hossain Faisal'
     })
 })
 
 app.get('/help', (req,res) => {
     res.render('help', {
-        email: 'mahbubhossain249@gmail.com'
+        email: 'mahbubhossain249@gmail.com',
+        title: 'Help',
+        name: 'Mahbub Hossain Faisal'
     })
 })
 
