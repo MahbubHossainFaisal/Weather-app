bc77f0629894d12b9be848442259ed302a283038
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-28 01:48:12 +0600

    Showing forecast and location to the browser and finalising user interface

--- Web-server/public/js/script.js
+++ Web-server/public/js/script.js
@@ -2,21 +2,36 @@ console.log('Client side javascript is loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne=document.querySelector('#message-1')
+const messageTwo=document.querySelector('#message-2')
 
 weatherForm.addEventListener('submit',(e)=>{
 
     e.preventDefault()
     const location = search.value
 
+    messageOne.textContent = 'Loading......'
+    messageTwo.textContent = ''
+    
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) =>{
 
     response.json().then((data)=>{
         if(data.error){
-            console.log(data.error)
+            const error=data.error
+            messageOne.textContent = ''
+            messageTwo.textContent = error
+            
         } else{
-            console.log(data.forecast)
-            console.log(data.location)
+
+            const tempurature = data.forecast.tempurature
+            const status = data.forecast.weatherStatus
+
+            messageOne.textContent = ('Tempurature: ')+ tempurature + (' degrees, Weather status: '+ status)
+            messageTwo.textContent = 'Location: '+ data.location
+           
         }
+        
     })
 })
 
