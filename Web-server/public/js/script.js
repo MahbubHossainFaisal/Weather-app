fa8648346cd58255c0fd046bafa316ef319a8fe9
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-01-28 00:36:00 +0600

    http request with fetch under script.js file

--- Web-server/public/js/script.js
+++ Web-server/public/js/script.js
@@ -1,1 +1,13 @@
 console.log('Client side javascript is loaded')
+
+fetch('http://localhost:3000/weather?address=Dhaka').then((response) =>{
+
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        } else{
+            console.log(data.forecast)
+            console.log(data.location)
+        }
+    })
+})
